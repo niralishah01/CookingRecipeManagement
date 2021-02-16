bec030f802bdf65b739670b66f0d911ba0b52f74
68777758+niralishah01@users.noreply.github.com <niralipshah2000@gmail.com>
2021-02-16 20:31:46 +0530

    ServiceHost is added.

---
+++ Zaayka-RecipePortal/ConsoleHost/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCFServices;
+
+namespace ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(AccountService), new Uri("http://localhost:8000/AccountService"));
+            host.Open();
+            Console.WriteLine("Service is hosted successfully.");
+            Console.ReadLine();
+            host.Close();
+        }
+    }
+}
