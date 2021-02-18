a7016f915bb31cc5525d2528ad006f3d671db4a5
Hena-123 <henapatel2000@gmail.com>
2021-02-18 23:17:27 +0530

    First commit

---
+++ WCFServices/WCFServices/Recipe.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFServices
+{
+    [DataContract]
+    public class Recipe
+    {
+        private int id;
+        private string title;
+        private string ingredients;
+        private string method;
+        private string image;
+        private string category;
+        private string otherdetails;
+        private int likes;
+        private int dislikes;
+        private int userID;
+
+        [DataMember]
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        [DataMember]
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        [DataMember]
+        public string Ingredients
+        {
+            get { return ingredients; }
+            set { ingredients = value; }
+        }
+
+        [DataMember]
+        public string Method
+        {
+            get { return method; }
+            set { method = value; }
+        }
+
+        [DataMember]
+        public string Image
+        {
+            get { return image; }
+            set { image = value; }
+        }
+
+        [DataMember]
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+
+        [DataMember]
+        public string Otherdetails
+        {
+            get { return otherdetails; }
+            set { otherdetails = value; }
+        }
+
+        [DataMember]
+        public int UserID
+        {
+            get { return userID; }
+            set { userID = value; }
+        }
+        [DataMember]
+        public int Likes
+        {
+            get { return likes; }
+            set { likes = value; }
+        }
+        [DataMember]
+        public int Dislikes
+        {
+            get { return dislikes; }
+            set { dislikes = value; }
+        }
+    }
+
+    
+}
---
+++ WCFServices/WebClient/home.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="home.aspx.cs" Inherits="WebClient.home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <style>
+        .imgThumbnail
+        {
+            height:200px;
+            width:auto;
+        }
+        @media only screen and (max-width:750px){
+            .card{
+                /*max-width: 100%;*/
+                display:block;
+                clear:both;
+            }        
+        }
+    </style>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <!-- For ICONS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+</head>
+<body>
+    <header>
+        <h2 class="bg-secondary p-2 text-light">
+                <img id="sitelogo" style="height:40px;width:40px" src="/Images/sitelogo.jpg" alt="No image">
+                Zaiyka
+                <a href="#" class="btn btn-light"><i class="fas fa-sign-out-alt"></i> Logout</a>
+        </h2>
+    </header>
+    <form id="form1" runat="server">
+        <div class="container p-1">
+            <asp:Panel ID="Panel1" runat="server">
+            </asp:Panel>
+        </div>
+    </form>
+</body>
+</html>
