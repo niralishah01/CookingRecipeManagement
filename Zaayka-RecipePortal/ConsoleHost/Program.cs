f647b776aa343f34c37774d2979eec309fb49af4
68777758+niralishah01@users.noreply.github.com <niralipshah2000@gmail.com>
2021-02-20 15:01:04 +0530

    All servicehost are added

--- Zaayka-RecipePortal/ConsoleHost/Program.cs
+++ Zaayka-RecipePortal/ConsoleHost/Program.cs
@@ -14,9 +14,16 @@ namespace ConsoleHost
         {
             ServiceHost host = new ServiceHost(typeof(AccountService), new Uri("http://localhost:8000/AccountService"));
             host.Open();
-            Console.WriteLine("Service is hosted successfully.");
+            Console.WriteLine("AccountService is hosted successfully.");
+            ServiceHost host1 = new ServiceHost(typeof(CommentService), new Uri("http://localhost:8080/CommentService"));
+            host1.Open();
+            Console.WriteLine("CommentService is hosted successfully.");
+            ServiceHost host2 = new ServiceHost(typeof(RecipeService), new Uri("http://localhost:8090/RecipeService"));
+            host2.Open();
+            Console.WriteLine("RecipeService is hosted successfully.");
             Console.ReadLine();
             host.Close();
+            host1.Close();
         }
     }
 }
