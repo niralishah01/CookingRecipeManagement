cfb056c09192bdd835111d300a1db2ac729c1c7e
68777758+niralishah01@users.noreply.github.com <niralipshah2000@gmail.com>
2021-02-21 22:36:41 +0530

    ConsoleHost is added.

---
+++ WCFProject_CE085_CE096_CE122/ConsoleHost/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host1 = new ServiceHost(typeof(WCFServices.RecipeService));
+            host1.Open();
+            Console.WriteLine("Published Recipe Service");
+            ServiceHost host2 = new ServiceHost(typeof(WCFServices.AccountService));
+            host2.Open();
+            Console.WriteLine("Published Account Service");
+            ServiceHost host3 = new ServiceHost(typeof(WCFServices.CommentService));
+            host3.Open();
+            Console.WriteLine("Published Comment Service");
+            Console.ReadLine();
+            host1.Close();
+            host2.Close();
+            host3.Close();
+        }
+    }
+}
